dc132436925d60c50294fb562d69bfd3fd2b38f4
AugustoCVS <guty.vertolis@gmail.com>
2023-06-28 10:36:45 -0300

    feat: create modal function

--- src/js/tokenMarket.js
+++ src/js/tokenMarket.js
@@ -43,83 +43,60 @@ document.addEventListener("DOMContentLoaded", () => {
             .then((lista_de_tokens) => {
             tokenList.innerHTML = "";
             for (let i in lista_de_tokens) {
+                const tokenId = lista_de_tokens[i].id;
                 tokenList.innerHTML += `
-            <div class="tokenOption">
-              <h3>${lista_de_tokens[i].title}</h3>
-              <p>R$: ${lista_de_tokens[i].price},00</p>
-              <p>Quantidade: ${lista_de_tokens[i].amount}</p>
-              <button class="btnBuyToken">Comprar</button>
-            </div>
-          `;
+              <div class="tokenOption">
+                <h3>${lista_de_tokens[i].title}</h3>
+                <p>R$: ${lista_de_tokens[i].price},00</p>
+                <p>Quantidade: ${lista_de_tokens[i].amount}</p>
+                <button class="btnBuyToken" data-id="${tokenId}">Comprar</button>
+              </div>
+            `;
             }
+            attachBuyTokenListeners();
         });
     }
     function saveTokenIdToLocalStorage(id) {
         localStorage.setItem('tokenId', id.toString());
     }
-    function getTokenIdFromApi(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield fetch("http://localhost:3000/tokens");
-            const tokens = yield response.json();
-            for (const token of tokens) {
-                if (id === token.id) {
-                    return Number(token.id);
-                }
-            }
-            throw new Error("Token not found");
+    function handleBuyTokenClick(event) {
+        const btnBuyToken = event.target;
+        const tokenId = btnBuyToken.dataset.id;
+        if (tokenId) {
+            saveTokenIdToLocalStorage(Number(tokenId));
+        }
+        else {
+            console.error('Token ID not found');
+        }
+    }
+    function attachBuyTokenListeners() {
+        const btnBuyTokens = document.querySelectorAll(".btnBuyToken");
+        btnBuyTokens.forEach((btnBuyToken) => {
+            btnBuyToken.addEventListener("click", handleBuyTokenClick, { once: true });
         });
     }
-    setInterval(createToken, 10 * 1000000); // Chama createToken a cada 10 segundos (em milissegundos)
-    setInterval(createTokenListFromApi, 10 * 10); // Chama createTokenListFromApi a cada 10 segundos (em milissegundos)
+    function createTokenSelectedModal() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const modal = document.getElementById("modal");
+            const tokenId = localStorage.getItem('tokenId');
+            if (tokenId) {
+                const response = yield fetch(`http://localhost:3000/tokens/${tokenId}`);
+                const tokenData = yield response.json();
+                const title = tokenData.title;
+                const price = tokenData.price;
+                const amount = tokenData.amount;
+                modal.innerHTML = `
+            <div class="modal-content">
+                <h3 id="modalTitle">${title}</h3>
+                <p id="modalPrice">R$${price},00</p>
+                <p id="modalAmount">Quantidade${amount}</p>
+                <input type="number" id="inputQuantity" placeholder="Quantidade">
+                <button id="btnConfirm">Confirmar</button>
+            </div>
+        `;
+            }
+        });
+    }
+    setInterval(createToken, 10 * 1000000);
+    setInterval(createTokenListFromApi, 10 * 10);
 });
-//   function tokenId() {
-//     const tokenOptions = document.getElementsByClassName("tokenOption");
-//     function handleBuyTokenClick(event: Event) {
-//       const btnBuyToken = event.target as HTMLButtonElement;
-//       const tokenOption = btnBuyToken.closest('.tokenOption');
-//       const index = Array.prototype.indexOf.call(tokenOptions, tokenOption);
-//       fetch("http://localhost:3000/tokens")
-//         .then((response) => response.json())
-//         .then(async (lista_de_tokens: any[]) => {
-//           const token = lista_de_tokens[index];
-//           const tokenId = await getTokenIdFromApi(token.id);
-//           saveTokenIdToLocalStorage(tokenId);
-//         });
-//     }
-//     for (let i = 0; i < tokenOptions.length; i++) {
-//       const btnBuyToken = tokenOptions[i].querySelector(".btnBuyToken") as HTMLButtonElement;
-//       btnBuyToken.addEventListener("click", handleBuyTokenClick);
-//     }
-//   }
-//   const btnBuyToken = document.getElementById('btnBuyToken') as HTMLButtonElement;
-//   btnBuyToken.addEventListener('click', tokenId);
-// async function createTokenSelectedModal(): Promise<void>{
-//     const divModal = document.getElementById("divModal") as HTMLDivElement;
-//     const tokenId = localStorage.getItem('tokenId')
-// }  
-//   async function createDivUserInfo(): Promise<void> {
-//     const userInfoFromApi = document.getElementById('userInfoFromApi') as HTMLDivElement;
-//     const userId = localStorage.getItem('userId');
-//     if(userStatus === 'true'){
-//         if (userId) {
-//           const response = await fetch(`http://localhost:3000/users/${userId}`);
-//           const userData = await response.json();
-//           const name = userData.nome;
-//           const balance = userData.saldoInicial;
-//           userInfoFromApi.innerText = `Nome: ${name} | Saldo: ${balance}`;
-//         } else {
-//         throw new Error("Usuário não encontrado");
-//       }
-//     }else{
-//       userInfoFromApi.innerText = ``;
-//     }
-//   }
-// <div id="modal" class="modal">
-//   <div class="modal-content">
-//     <h3 id="modalTitle"></h3>
-//     <p id="modalPrice"></p>
-//     <p id="modalAmount"></p>
-//     <input type="number" id="inputQuantity" placeholder="Quantidade">
-//     <button id="btnConfirm">Confirmar</button>
-//   </div>
-// </div>
--- src/ts/tokenMarket.ts
+++ src/ts/tokenMarket.ts
@@ -1,142 +1,113 @@
 // import { TokenClass } from "./classes/tokenClass";
 
 class TokenClass {
-  public title: string;
-  public price: number;
-  public amount: number;
-
-  constructor(title: string) {
-    this.title = title;
-    this.price = 0;
-    this.amount = 0;
+    public title: string;
+    public price: number;
+    public amount: number;
+  
+    constructor(title: string) {
+      this.title = title;
+      this.price = 0;
+      this.amount = 0;
+    }
+  
+    public generateRandomValues() {
+      this.price = Math.floor(100 * Math.random());
+      this.amount = Math.floor(100 * Math.random());
+    }
   }
-
-  public generateRandomValues() {
-    this.price = Math.floor(100 * Math.random());
-    this.amount = Math.floor(100 * Math.random());
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  function createToken() {
-    const token = new TokenClass('Token');
-    token.generateRandomValues();
-
-    fetch("http://localhost:3000/tokens", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(token)
-    })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  function createTokenListFromApi() {
-    const tokenList = document.getElementsByClassName('tokenList')[0] as HTMLElement;
-
-    fetch("http://localhost:3000/tokens")
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    function createToken() {
+      const token = new TokenClass('Token');
+      token.generateRandomValues();
+  
+      fetch("http://localhost:3000/tokens", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(token)
+      })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  
+    function createTokenListFromApi() {
+      const tokenList = document.getElementsByClassName('tokenList')[0] as HTMLElement;
+  
+      fetch("http://localhost:3000/tokens")
         .then(response => response.json())
-      .then((lista_de_tokens: any[]) => {
-        tokenList.innerHTML = ""; 
-
-        for (let i in lista_de_tokens) {
-          tokenList.innerHTML += `
-            <div class="tokenOption">
-              <h3>${lista_de_tokens[i].title}</h3>
-              <p>R$: ${lista_de_tokens[i].price},00</p>
-              <p>Quantidade: ${lista_de_tokens[i].amount}</p>
-              <button class="btnBuyToken">Comprar</button>
-            </div>
-          `;
-        }
-      });
-  }
-
-  function saveTokenIdToLocalStorage(id: number) {
-    localStorage.setItem('tokenId', id.toString());
-  }
+        .then((lista_de_tokens: any[]) => {
+          tokenList.innerHTML = "";
   
-  async function getTokenIdFromApi(id: number): Promise<number> {
-    const response = await fetch("http://localhost:3000/tokens");
-    const tokens = await response.json();
+          for (let i in lista_de_tokens) {
+            const tokenId = lista_de_tokens[i].id;
+            tokenList.innerHTML += `
+              <div class="tokenOption">
+                <h3>${lista_de_tokens[i].title}</h3>
+                <p>R$: ${lista_de_tokens[i].price},00</p>
+                <p>Quantidade: ${lista_de_tokens[i].amount}</p>
+                <button class="btnBuyToken" data-id="${tokenId}">Comprar</button>
+              </div>
+            `;
+          }
   
-    for (const token of tokens) {
-      if (id === token.id) {
-        return Number(token.id);
+          attachBuyTokenListeners();
+        });
+    }
+  
+    function saveTokenIdToLocalStorage(id: number) {
+      localStorage.setItem('tokenId', id.toString());
+    }
+  
+    function handleBuyTokenClick(event: Event): void {
+      const btnBuyToken = event.target as HTMLButtonElement;
+      const tokenId = btnBuyToken.dataset.id;
+  
+      if (tokenId) {
+        saveTokenIdToLocalStorage(Number(tokenId));
+      } else {
+        console.error('Token ID not found');
       }
     }
-    throw new Error("Token not found");
-  }
-
   
-  setInterval(createToken, 10 * 1000000); // Chama createToken a cada 10 segundos (em milissegundos)
-  setInterval(createTokenListFromApi, 10 * 10); // Chama createTokenListFromApi a cada 10 segundos (em milissegundos)
-});
+    function attachBuyTokenListeners() {
+      const btnBuyTokens = document.querySelectorAll(".btnBuyToken");
+      btnBuyTokens.forEach((btnBuyToken) => {
+        btnBuyToken.addEventListener("click", handleBuyTokenClick, { once: true });
+      });
+    }
 
-//   function tokenId() {
-//     const tokenOptions = document.getElementsByClassName("tokenOption");
-  
-//     function handleBuyTokenClick(event: Event) {
-//       const btnBuyToken = event.target as HTMLButtonElement;
-//       const tokenOption = btnBuyToken.closest('.tokenOption');
-//       const index = Array.prototype.indexOf.call(tokenOptions, tokenOption);
-  
-//       fetch("http://localhost:3000/tokens")
-//         .then((response) => response.json())
-//         .then(async (lista_de_tokens: any[]) => {
-//           const token = lista_de_tokens[index];
-//           const tokenId = await getTokenIdFromApi(token.id);
-//           saveTokenIdToLocalStorage(tokenId);
-//         });
-//     }
-  
-//     for (let i = 0; i < tokenOptions.length; i++) {
-//       const btnBuyToken = tokenOptions[i].querySelector(".btnBuyToken") as HTMLButtonElement;
-//       btnBuyToken.addEventListener("click", handleBuyTokenClick);
-//     }
-//   }
+    async function createTokenSelectedModal(): Promise<void>{
 
-//   const btnBuyToken = document.getElementById('btnBuyToken') as HTMLButtonElement;
-//   btnBuyToken.addEventListener('click', tokenId);
-
-// async function createTokenSelectedModal(): Promise<void>{
-
-//     const divModal = document.getElementById("divModal") as HTMLDivElement;
-//     const tokenId = localStorage.getItem('tokenId')
+    const modal = document.getElementById("modal") as HTMLDivElement;
+    const tokenId = localStorage.getItem('tokenId')
     
-// }  
+    if(tokenId){
+        const response = await fetch(`http://localhost:3000/tokens/${tokenId}`);
+        const tokenData = await response.json()
 
-//   async function createDivUserInfo(): Promise<void> {
-//     const userInfoFromApi = document.getElementById('userInfoFromApi') as HTMLDivElement;
-//     const userId = localStorage.getItem('userId');
+        const title = tokenData.title;
+        const price = tokenData.price;
+        const amount = tokenData.amount;
+
+        modal.innerHTML = `
+            <div class="modal-content">
+                <h3 id="modalTitle">${title}</h3>
+                <p id="modalPrice">R$${price},00</p>
+                <p id="modalAmount">Quantidade${amount}</p>
+                <input type="number" id="inputQuantity" placeholder="Quantidade">
+                <button id="btnConfirm">Confirmar</button>
+            </div>
+        `
+        }
+
+    }  
   
-//     if(userStatus === 'true'){
-//         if (userId) {
-//           const response = await fetch(`http://localhost:3000/users/${userId}`);
-//           const userData = await response.json();
-      
-//           const name = userData.nome;
-//           const balance = userData.saldoInicial;
-      
-//           userInfoFromApi.innerText = `Nome: ${name} | Saldo: ${balance}`;
-//         } else {
-//         throw new Error("Usuário não encontrado");
-//       }
-//     }else{
-//       userInfoFromApi.innerText = ``;
-//     }
-//   }
-
-// <div id="modal" class="modal">
-//   <div class="modal-content">
-//     <h3 id="modalTitle"></h3>
-//     <p id="modalPrice"></p>
-//     <p id="modalAmount"></p>
-//     <input type="number" id="inputQuantity" placeholder="Quantidade">
-//     <button id="btnConfirm">Confirmar</button>
-//   </div>
-// </div>
+    setInterval(createToken, 10 * 1000000);
+    setInterval(createTokenListFromApi, 10 * 10);
+  });
+  
